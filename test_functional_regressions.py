93fc18deb971def792a56e33d09fe3931e1bcda5
David Corvoysier <david@huggingface.co>
2023-06-16 16:05:15 +0000

    Imported a Triton unit test to check their support for int8 matmul

---
+++ test_functional_regressions.py
@@ -0,0 +1,127 @@
+import numpy as np
+import pytest
+import torch
+from numpy.random import RandomState
+
+import triton
+import triton.language as tl
+
+def _test_matmul(a, b,
+                 m, block_m, n, block_n, k, block_k, group_size_m,
+                 lhs_dtype, rhs_dtype):
+
+    @triton.jit
+    def matmul_kernel(
+        # Pointers to matrices
+        a_ptr, b_ptr, c_ptr,
+        # Matrix dimensions
+        M, N, K,
+        # The stride variables represent how much to increase the ptr by when moving by 1
+        # element in a particular dimension. E.g. `stride_am` is how much to increase `a_ptr`
+        # by to get the element one row down (A has M rows).
+        stride_am, stride_ak,
+        stride_bk, stride_bn,
+        stride_cm, stride_cn,
+        # Meta-parameters
+        BLOCK_SIZE_M: tl.constexpr, BLOCK_SIZE_N: tl.constexpr, BLOCK_SIZE_K: tl.constexpr,
+        GROUP_SIZE_M: tl.constexpr,
+        IS_LHS_FP8: tl.constexpr,
+        IS_RHS_FP8: tl.constexpr,
+    ):
+        """Kernel for computing the matmul C = A x B.
+        A has shape (M, K), B has shape (K, N) and C has shape (M, N)
+        """
+        # -----------------------------------------------------------
+        # Map program ids `pid` to the block of C it should compute.
+        # This is done in a grouped ordering to promote L2 data reuse.
+        # See above `L2 Cache Optimizations` section for details.
+        pid = tl.program_id(axis=0)
+        num_pid_m = tl.cdiv(M, BLOCK_SIZE_M)
+        num_pid_n = tl.cdiv(N, BLOCK_SIZE_N)
+        num_pid_in_group = GROUP_SIZE_M * num_pid_n
+        group_id = pid // num_pid_in_group
+        first_pid_m = group_id * GROUP_SIZE_M
+        group_size_m = min(num_pid_m - first_pid_m, GROUP_SIZE_M)
+        pid_m = first_pid_m + (pid % group_size_m)
+        pid_n = (pid % num_pid_in_group) // group_size_m
+
+        # ----------------------------------------------------------
+        # Create pointers for the first blocks of A and B.
+        # We will advance this pointer as we move in the K direction
+        # and accumulate
+        # `a_ptrs` is a block of [BLOCK_SIZE_M, BLOCK_SIZE_K] pointers
+        # `b_ptrs` is a block of [BLOCK_SIZE_K, BLOCK_SIZE_N] pointers
+        # See above `Pointer Arithmetics` section for details
+        offs_am = (pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)) % M
+        offs_bn = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % N
+        offs_k = tl.arange(0, BLOCK_SIZE_K)
+        a_ptrs = a_ptr + (offs_am[:, None] * stride_am + offs_k[None, :] * stride_ak)
+        b_ptrs = b_ptr + (offs_k[:, None] * stride_bk + offs_bn[None, :] * stride_bn)
+
+        # -----------------------------------------------------------
+        # Iterate to compute a block of the C matrix.
+        # We accumulate into a `[BLOCK_SIZE_M, BLOCK_SIZE_N]` block
+        # of fp32 values for higher accuracy.
+        # `accumulator` will be converted back to fp16 after the loop.
+        accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+        for k in range(0, tl.cdiv(K, BLOCK_SIZE_K)):
+            # Load the next block of A and B, generate a mask by checking the K dimension.
+            # If it is out of bounds, set it to 0.
+            a = tl.load(a_ptrs, mask=offs_k[None, :] < K - k * BLOCK_SIZE_K, other=0.0)
+            if IS_LHS_FP8:
+              a = a.to(tl.float8e5, bitcast=True)
+            a = a.to(tl.float16)
+            b = tl.load(b_ptrs, mask=offs_k[:, None] < K - k * BLOCK_SIZE_K, other=0.0)
+            if IS_RHS_FP8:
+              b = b.to(tl.float8e5, bitcast=True)
+            b = b.to(tl.float16)
+            # We accumulate along the K dimension.
+            accumulator += tl.dot(a, b)
+            # Advance the ptrs to the next K block.
+            a_ptrs += BLOCK_SIZE_K * stride_ak
+            b_ptrs += BLOCK_SIZE_K * stride_bk
+        c = accumulator.to(tl.float16)
+
+        # -----------------------------------------------------------
+        # Write back the block of the output matrix C with masks.
+        offs_cm = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+        offs_cn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+        c_ptrs = c_ptr + stride_cm * offs_cm[:, None] + stride_cn * offs_cn[None, :]
+        c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
+        tl.store(c_ptrs, c, mask=c_mask)
+
+    grid = (triton.cdiv(m, block_m) * triton.cdiv(n, block_n),)
+    triton_output = torch.empty((m, n), device=a.device, dtype=torch.float16)
+    matmul_kernel[grid](
+        a, b, triton_output,
+        m, n, k,
+        a.stride(0), a.stride(1),
+        b.stride(0), b.stride(1),
+        triton_output.stride(0), triton_output.stride(1),
+        block_m, block_n, block_k,
+        group_size_m,
+        IS_LHS_FP8=(lhs_dtype == "float8"),
+        IS_RHS_FP8=(rhs_dtype == "float8")
+    )
+    return triton_output
+
+
+@pytest.mark.parametrize("reference", ["torch", "torch_int"])
+def test_matmul_int8(reference):
+    m, n, k = 32, 64, 128
+    block_m, block_n, block_k = 16, 32, 64
+    group_size_m = 8
+
+    def get_input(n, m):
+      return torch.randn((n, m), device='cuda', dtype=torch.float16).to(torch.int8)
+
+    torch.manual_seed(0)
+
+    a = get_input(m, k)
+    b = get_input(k, n)
+    triton_output = _test_matmul(a, b,
+                                 m, block_m, n, block_n, k, block_k, group_size_m,
+                                 "int8", "int8")
+    if reference == "torch":
+        ref_output = torch.matmul(a.to(torch.float16), b.to(torch.float16))
+        assert torch.allclose(triton_output, ref_output, atol=1e-2, rtol=1e-2)
